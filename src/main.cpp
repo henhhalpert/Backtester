9e06f3121a882351e802f06d06ab0541bc6d5a85
Hen Halpert <henhhalpert@gmail.com>
2025-04-15 17:49:46 +0300

    Removing old python tests.

--- src/main.cpp
+++ src/main.cpp
@@ -6,69 +6,6 @@
 #include <Modules/Data/DatabaseHandler.hpp>
 #include <Modules/PyIntegration/PythonBridge.hpp>
 
-void run_eda()
-{       
-    try
-    {
-       /* py::module sys = py::module::import("sys");
-        sys.attr("path").attr("insert")(0, PYTHON_DIR);
-        py::module runner = py::module::import("runner");
-        runner.attr("run")(pythonTaskToString(PythonTasks::EDA));*/
-
-        // SCRIPTING FROM c++ 
-    //    Py_Initialize();
-    //    py::module sys = py::module::import("sys");
-    //    py::module runner = py::module::import("runner");
-    //    runner.attr("run")("eda");
-
-    //    py::exec(R"(
-    //    import pandas as pd
-    //    import matplotlib.pyplot as plt
-    //    import psycopg2
-    //    from matplotlib.dates import DateFormatter
-    //    
-    //    # DB config
-    //    DB_NAME = "backtester_local"
-    //    DB_USER = "postgres"
-    //    DB_PASS = "1111"
-    //    DB_HOST = "localhost"
-    //    DB_PORT = "5432"
-    //    
-    //    # Connect and query
-    //    conn = psycopg2.connect(
-    //        dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
-    //    )
-    //    
-    //    query = """
-    //        SELECT time, open, high, low, close, volume
-    //        FROM ohlcv_data
-    //        ORDER BY time ASC
-    //        LIMIT 1000;
-    //    """
-    //    
-    //    df = pd.read_sql(query, conn, parse_dates=['time'])
-    //    conn.close()
-    //    
-    //    # Plot
-    //    plt.figure(figsize=(12, 6))
-    //    plt.plot(df['time'], df['close'], label='Close Price')
-    //    plt.title("OHLCV Close Prices")
-    //    plt.xlabel("Time")
-    //    plt.ylabel("Price")
-    //    plt.grid(True)
-    //    plt.legend()
-    //    plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M'))
-    //    plt.xticks(rotation=45)
-    //    plt.tight_layout()
-    //    plt.show()
-    //)");
-    }
-    catch (const py::error_already_set& e)
-    {
-        std::cerr << "Python Error: " << e.what() << std::endl;
-    }
-}
-
 int main(int argc, char** argv)
 {
     std::cout << "HelloCpp!\n";
